d886b3f42a4d4d729662362442ac5f886b5c7477
Neeff <dpincheira2016@alu.uct.cl>
2019-03-13 20:38:34 -0300

    add: export to csv file data Shops activeadmin

--- app/admin/shops.rb
+++ app/admin/shops.rb
@@ -14,4 +14,36 @@ ActiveAdmin.register Shop do
 permit_params :name, :description, :user_id
 
 filter :name
+csv do
+  column :email do |shop|
+    shop.user.email
+  end
+  column :amount do |shop|
+    orders = shop.orders.where(paid: true).where(dispached: true).where('created_at >= :month', month: Time.now - 30.days).map{|order| order.quantity * order.price}.sum 
+  end
+  column :currency do
+    'USD'
+  end
+  column :id
+  column :note do
+    'payment'
+  end
+
+end
+
+
+
+
+show do
+    attributes_table do
+      row :name
+      row :user_id
+      row :created_at
+      row :updated_at
+    end
+    attributes_table do
+       span "#{Shop.find(params[:id])} "
+    end  
+end
+
 end
